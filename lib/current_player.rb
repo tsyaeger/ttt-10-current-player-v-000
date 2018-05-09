e8a65df4fa2b9b0336134ee4019034358d7560c8
taryn yaeger <tsyaeger@gmail.com>
2018-05-09 04:19:41 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+
+
+def turn_count(board)
+  x_count = 0
+  board.each do |cell|
+    if cell == "X"
+      x_count += 1
+  
+end
+
+
+
+def current_player(board)
+  turn = turn_count(board)
+
+  if turn 
+    
+  else
+    
+end
